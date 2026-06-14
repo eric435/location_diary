6385fd208ca56eb60b6d4abe5e7bed7a44f3b8cc
Eric Moller <emoller@outlook.com>
2026-06-14 08:24:00 -0600

    primevue UI library install

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,13 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Button from 'primevue/button'
+
+const simplealert = () => alert('Button was clicked!')
+</script>
 
 <template>
   <h1>You did it!</h1>
+
+  <Button v-on:click="simplealert">Click me for fun</Button>
   <p>
     Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
     documentation
